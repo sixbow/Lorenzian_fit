f81a0be55ef43cf058a69fd3d5ffb43bbb15ff85
Sietse desktop <sietsedeboer22@hotmail.com>
2022-03-17 19:46:25 +0100

    Working version of fitting code!

--- Lorenzian_fit_Main.py
+++ Lorenzian_fit_Main.py
@@ -10,14 +10,20 @@ plt.rcParams["figure.dpi"] = 300 #Makes the plots render in 300dpi
 #%% Loading data and importing data
 # Loading in the data -> See Import.md for info on how to perform an import from sonnet.
 cwd = os.getcwd()
-datafolder = cwd + r'\datafolder'
-Ocoupler_overlap = 4.0 ;
-Ocoupler_str = str(Ocoupler_overlap)
-Ocoupler_overlap_str = "{:2.1f}".format(Ocoupler_overlap)
-print(Ocoupler_str)
+datafolder = cwd + r'\datafolder\MWOffice_Output_csv'
+Group = 1;
+Appc_to_be_squared = 50.0;
+Oeff = 4.50 ;
+
+Group_str = "{:1.0f}".format(Group)
+Appc_to_be_squared_str = "{:2.1f}".format(Appc_to_be_squared)
+Oeff_str = "{:1.2f}".format(Oeff)
+print(Group_str)
+print(Appc_to_be_squared_str)
+print(Oeff_str)
 
 #---> Insert your datapath here! You can uncomment lines to use different data
-measurement = '\MW2500_2-2-2_Oeff4_0' + '.txt'#4 Coupler data
+measurement = '\MWG'+ Group_str + '_'+ Appc_to_be_squared_str +'_Oeff_' + Oeff_str +'.txt'#4 Coupler data
 print(measurement)
 #---> skip controls the amount of header lines to ignore
 skip = 15;#Old data 13
@@ -47,10 +53,30 @@ evaluation = model.eval(params=result.params, f=f)
 fig, ax = plt.subplots()
 ax.scatter(f,S21_dB, s=1, label='Data')
 ax.plot(f,evaluation, color='green', label='Fit')
-ax.set_title('...'+measurement[(len(measurement)-15):len(measurement)])
+ax.set_title('G' + Group_str + '|' +'$\sqrt{A}$ =' +Appc_to_be_squared_str + '|' + 'O_{eff}= ' + Oeff_str + '->' +'Q_{c} = ' + "{:2.2e}".format(Qc))
 ax.set_xlim([f0-10*f0/Ql, f0+10*f0/Ql])
 ax.set_xlabel('F (GHz)')
 ax.set_ylabel('|S21| [dB]')
 ax.legend()
-print('Oc_overlap = ' + Ocoupler_overlap_str + '\n Ql = '+str(Ql)+'\n Qc = ' + str(Qc) + '\n Qi = ' + str(Qi) + '\n f0 = ' + str(f0) ) 
+
+#Save Each figure:
+plt.savefig(cwd + "\Images\SiC_final_fitting\\" + measurement[1:-4] + '.png')
+
+print('Oc_overlap = ' + Oeff_str + '\n Ql = '+str(Ql)+'\n Qc = ' + str(Qc) + '\n Qi = ' + str(Qi) + '\n f0 = ' + str(f0) ) 
+data_str = measurement + '\t' + Group_str + '\t' + Appc_to_be_squared_str + '\t' + Oeff_str + '\t' + "{:1.5f}".format(f0) + '\t' + "{:1.3e}".format(Ql) + '\t' + "{:1.3e}".format(Qc) + '\t' + "{:1.3e}".format(Qi) 
+text = [ data_str ]
+with open(cwd + "\Output_data\\" + 'SiC_fitting_data_append.csv','a') as file:
+    for line in text:
+        file.write(line)
+        file.write('\n')
+# Now we want to save these result to a text file!
+
+
+
+
+
+
+
+
+
 
